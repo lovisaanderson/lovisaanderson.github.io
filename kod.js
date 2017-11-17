2cbf5b007da13c3590068e30e2e161839e7a6027
lovisaanderson <lovisaanderson@icloud.com>
2017-11-17 12:49:15 +0100

    d

--- kod.js
+++ kod.js
@@ -2,4 +2,13 @@ $(function(){
     $("#openinfo").click(function(){
         $("#info").toggle();
     });
+    $("li").dblclick(function(){ 
+        $(this).toggleClass( "selected" ); 
+    });
+    $("#bgcolor").change(function(){
+        $("body").css("background-color",$(this).val());
+    });
+     $("#tecolor").change(function(){
+        $("body").css("color",$(this).val());
+    });
 });
